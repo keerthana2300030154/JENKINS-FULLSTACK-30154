bc12817d32827d7d5ab7837882eb95a7c3bfbaec
Keerthana <darapukeerthana2808@gmail.com>
2025-09-06 05:40:52 +0530

    modified frontend

--- EMPLOYEEAPI-REACT/employeeapi-react/vite.config.js
+++ EMPLOYEEAPI-REACT/employeeapi-react/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/employeeapi-frontend/',
   plugins: [react()],
-  base: '/springbootemployeeapi/',   // affects production build paths
-  server: {
-    port: 5173,
-    open: '/springbootemployeeapi/', // automatically opens this URL
-  }
 })
